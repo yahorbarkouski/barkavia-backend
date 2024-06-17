b9b855c39f173d053ee1d09c080197e8d189f1e2
Yahor Barkouski <yahor.barkouski@gmail.com>
2024-06-17 13:59:38 +0200

    api: exception handler

---
+++ barkavia-web-api/src/main/kotlin/com/barkavia/api/config/GlobalExceptionHandler.kt
@@ -0,0 +1,47 @@
+package com.barkavia.api.config
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
+
+@ControllerAdvice
+internal class GlobalExceptionHandler {
+
+    private val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+    @ExceptionHandler(
+        IllegalArgumentException::class,
+        MethodArgumentTypeMismatchException::class,
+        IllegalStateException::class
+    )
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun acceptNoGuiltExceptionHandler(e: RuntimeException): ResponseEntity<ErrorResponse> {
+        log.error("Runtime exceptions occurred", e)
+        return "Sorry, we can't process your request.".asErrorResponse(HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.OK)
+    fun acceptNoErrorsExceptionHandler(e: Exception): ResponseEntity<ErrorResponse> {
+        log.error("Unhandled exception occurred", e)
+        return "Please try again later :)".asErrorResponse(HttpStatus.OK)
+    }
+
+    private fun String.asErrorResponse(status: HttpStatus): ResponseEntity<ErrorResponse> {
+        val errorResponse = ErrorResponse(
+            status = status.value(),
+            message = this
+        )
+        return ResponseEntity(errorResponse, status)
+    }
+}
+
+internal data class ErrorResponse(
+    val status: Int,
+    val message: String
+)
