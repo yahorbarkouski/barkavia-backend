8f0f61e0d6f574065bd530ed01298adda0575909
Yahor Barkouski <yahor.barkouski@gmail.com>
2024-06-17 14:11:45 +0200

    build: remove spring build

--- barkavia-airline-clients/build.gradle.kts
+++ barkavia-airline-clients/build.gradle.kts
@@ -1,18 +1,17 @@
 plugins {
     kotlin("jvm")
     kotlin("plugin.spring")
-    id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-json")
-    implementation("org.springframework:spring-tx")
+    implementation("org.springframework.boot:spring-boot-starter-json:3.3.0")
+    implementation("org.springframework:spring-tx:6.1.9")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
     testImplementation("org.springframework.cloud:spring-cloud-contract-wiremock:4.1.3")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.0")
 }
