bf13c20b6be5954e9a0f43e67572608c21488827
Yahor Barkouski <yahor.barkouski@gmail.com>
2024-06-17 13:59:31 +0200

    api: webmvc config

---
+++ barkavia-web-api/src/main/kotlin/com/barkavia/api/config/WebMvcConfiguration.kt
@@ -0,0 +1,23 @@
+package com.barkavia.api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfiguration {
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000", "https://barkavia.com", "https://vercel.barkavia.app")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
+    }
+}
